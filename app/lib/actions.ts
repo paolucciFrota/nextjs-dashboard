f5b11d815e4bbe564ffe848505b978c1f8c54b36
paolucciFrota <gabrielpaoluccifrota@gmail.com>
2025-03-19 17:45:15 -0300

    removed sql

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -24,10 +24,6 @@ export async function createInvoice(formData: FormData) {
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0];
  
-    await sql`
-        INSERT INTO invoices (customer_id, amount, status, date)
-        VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
-    `;
 
     revalidatePath('/dashboard/invoices')
     redirect('/dashboard/invoices');
